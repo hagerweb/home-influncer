4750dd8c9ab87208461fa9ec97647d6d1eb2db09
HaGer <haggerhamdy75@gmail.com>
2024-07-30 16:57:30 +0300

    initial commit

---
+++ Js/mine.js
@@ -0,0 +1,10 @@
+// $(".navbar a").click(function(eInfo){
+
+//      let y = $(eInfo.target).attr('href');
+
+//      let x = $(y).offset().top;
+    
+//     $("html,body").animate({scrollTop : x },2000)
+   
+//     console.log(y);
+// })
